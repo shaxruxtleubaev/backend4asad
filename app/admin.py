b1363858188541360c89cfd91e295d0990df29c7
shaxruxtleubaev <tleubaevshaxrux@outlook.com>
2023-08-24 11:34:34 +0500

    removing uz-ru-en categories in the admin panel

--- app/admin.py
+++ app/admin.py
@@ -15,12 +15,12 @@ class EventAdmin(TranslationAdmin):
 	list_display_links = ('id', 'title', 'date', 'time')
 	ordering = ('-date',)
 
-	class Media:
-		js = (
-            'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
-            'http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js',
-            'modeltranslation/js/tabbed_translation_fields.js',
-        )
-		css = {
-            'screen': ('modeltranslation/css/tabbed_translation_fields.css',),
-        }
+	# class Media:
+	# 	js = (
+    #         'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
+    #         'http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js',
+    #         'modeltranslation/js/tabbed_translation_fields.js',
+    #     )
+	# 	css = {
+    #         'screen': ('modeltranslation/css/tabbed_translation_fields.css',),
+    #     }
